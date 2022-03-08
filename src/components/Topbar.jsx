bac87406214d2ddcd930c0586ed9ce3ef0741140
Mahdi Parivash <mahdiparivsh@protonmail.com>
2022-03-08 12:38:40 +0330

    Finish Topbar Component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import "../style/Topbar.css";
-
+import avatar from "../image/avatar.jpg";
 function Topbar() {
   return (
     <div className="top">
       <div className="topRight">
-        <i class="fa-brands fa-facebook"></i>
-        <i class="fa-brands fa-twitter"></i>
-        <i class="fa-brands fa-instagram"></i>
-        <i class="fa-brands fa-telegram"></i>
+        <i className="topIcon fa-brands fa-facebook"></i>
+        <i className="topIcon fa-brands fa-twitter"></i>
+        <i className="topIcon fa-brands fa-instagram"></i>
+        <i className="topIcon fa-brands fa-telegram"></i>
       </div>
-      <div className="topCenter"></div>
-      <div className="topLeft"></div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">خروج</li>
+          <li className="topListItem">تماس با ما</li>
+          <li className="topListItem">خانه</li>
+          <li className="topListItem">نوشتن</li>
+          <li className="topListItem">درباره ما</li>
+        </ul>
+      </div>
+      <div className="topLeft">
+        <img className="topImg" src={avatar} alt="" />
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   );
 }
