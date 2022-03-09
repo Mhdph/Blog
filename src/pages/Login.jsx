c959bc558b3483b9331b8631e66df03508e11479
Mahdi Parivash <mahdiparivsh@protonmail.com>
2022-03-09 16:45:23 +0330

    Login Page Finished

---
+++ src/pages/Login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../style/Login.css";
+export default function Login() {
+  return (
+    <div className="login">
+      <span className="loginTittle">ورود به حساب کاربری</span>
+      <form className="loginForm">
+        <label>پست الکترونیکی</label>
+        <input
+          className="loginInput"
+          type="text"
+          placeholder="پست الکترونیکی خود را بنویسید"
+        />
+        <label> رمز عبور</label>
+        <input
+          className="loginInput"
+          type="text"
+          placeholder=" رمز عبور خود را بنویسید"
+        />
+        <button className="loginButton">ورود</button>
+        <span className="forgetPassword">
+          <a href="">رمز عبور خود را فراموش کرده اید؟</a>
+        </span>
+        <button className="loginRegisterButton">ثبت نام</button>
+      </form>
+    </div>
+  );
+}
